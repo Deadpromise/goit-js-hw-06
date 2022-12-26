5c3480c6d6120b8f99bfda2706e3e6520de079b5
Dmitry Yavtushenko <rosengan22@gmail.com>
2022-12-26 02:04:54 +0200

    8 tasks done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+// alert("sdsd");
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    const {
+        elements: {email, password}
+    } = e.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('All fields must be filled')
+    };
+
+    const formData = {
+        email: email.value,
+        password: password.value,
+    };
+    console.log(formData);
+
+    e.currentTarget.reset();
+
+};
