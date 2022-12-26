e5d820eb081da68ed46a2dbb4e92332a196928a2
Dmitry Yavtushenko <rosengan22@gmail.com>
2022-12-26 02:36:12 +0200

    9 tasks done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const butRef = document.querySelector('.change-color');
+const textRef = document.querySelector('.color');
+
+
+butRef.addEventListener('click', changeColor);
+
+function changeColor() {
+  const currentColor = getRandomHexColor();
+  document.body.style.backgroundColor = currentColor;
+  textRef.textContent = currentColor;
+};
+
